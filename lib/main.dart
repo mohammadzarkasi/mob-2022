b64aecb406a818aded7199ffec69b27e2c1f9ac3
mozar <mozar@mozars-MacBook-Pro.local>
2022-05-09 20:34:50 +0700

    halaman login

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 import 'package:mob2022/screen_confirm.dart';
 
@@ -9,54 +11,151 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: ScreenSignup(),
+      home: ScreenSignin(),
     );
   }
 }
 
-class ScreenSignup extends StatelessWidget {
-  ScreenSignup({
+class ScreenSignin extends StatefulWidget {
+  ScreenSignin({
     Key? key,
   }) : super(key: key);
 
+  @override
+  State<ScreenSignin> createState() => _ScreenSigninState();
+}
+
+class _ScreenSigninState extends State<ScreenSignin> {
   String username = 'agus', password = '123456';
+
+  var ctrlUsername = TextEditingController();
+
+  var ctrlPassword = TextEditingController();
+
+  var formkey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.yellow,
-      appBar: AppBar(
-        backgroundColor: Colors.red,
-        title: Text('Ini Judul'),
-      ),
-      body: Column(
-        children: [
-          Text('ini text 1'),
-          Text('ini text 2'),
-          Text('ini text 3'),
-          Text('ini text 4'),
-          Text('ini text 5'),
-          ElevatedButton(
-            // anonimous function
-            onPressed: () async {
-              // print('tombol ditekan');
-              var res = await Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (BuildContext ctx) {
-                    return ScreenConfirmation(
-                      username: username,
-                      password: password,
-                    );
-                  },
-                ),
-              );
-              print('hasil dari konfirmasi adalah: $res');
-            },
-            child: Text('Tekan disini'),
-            // child: Icon(Icons.delete),
+      body: SafeArea(
+        child: Container(
+          decoration: BoxDecoration(
+            color: Color.fromRGBO(229, 229, 229, 1),
           ),
-        ],
+          width: double.infinity,
+          alignment: Alignment.topCenter,
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                Image.asset('assets/imgs/icon.png'),
+                Text('Agristore'),
+                SizedBox(
+                  height: 80,
+                ),
+                _buildFormLogin(),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildFormLogin() {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 70),
+      child: Form(
+        key: formkey,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text('Username'),
+            SizedBox(
+              height: 5,
+            ),
+            TextFormField(
+              controller: ctrlUsername,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                contentPadding: EdgeInsets.symmetric(
+                  horizontal: 5,
+                ),
+                // focusColor: Color.fromRGBO(206, 255, 188, 1),
+                // hoverColor: Color.fromRGBO(206, 255, 188, 1),
+                fillColor: Color.fromRGBO(206, 255, 188, 1),
+                filled: true,
+                prefixIcon: Image.asset(
+                  'assets/imgs/profil.png',
+                  height: 12,
+                ),
+              ),
+              validator: (val) {
+                if (val == null) {
+                  return 'tidak boleh kosong';
+                }
+                if (val.length < 3) {
+                  return 'Minimal 3 karakter';
+                }
+                return null;
+              },
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text('Password'),
+            SizedBox(
+              height: 5,
+            ),
+            TextFormField(
+              controller: ctrlPassword,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                contentPadding: EdgeInsets.symmetric(
+                  horizontal: 5,
+                ),
+                fillColor: Color.fromRGBO(206, 255, 188, 1),
+                filled: true,
+              ),
+              obscureText: true,
+              validator: (val) {
+                if (val == null) {
+                  return 'password harus diisi';
+                }
+                if (val.length < 6) {
+                  return 'password minimal 6 karakter';
+                }
+                return null;
+              },
+            ),
+            SizedBox(
+              height: 40,
+            ),
+            Container(
+              width: double.infinity,
+              alignment: Alignment.center,
+              child: ElevatedButton(
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(Colors.white),
+                  foregroundColor: MaterialStateProperty.all(Colors.black),
+                  padding: MaterialStateProperty.all(
+                    EdgeInsets.symmetric(horizontal: 25),
+                  ),
+                ),
+                onPressed: () {
+                  if (formkey.currentState != null) {
+                    if (formkey.currentState!.validate()) {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(content: Text('Berhasil login')));
+                    } else {}
+                  }
+                },
+                child: Text(
+                  'LOGIN',
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
